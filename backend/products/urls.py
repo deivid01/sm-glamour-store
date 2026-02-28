3f080029725eeabd948f7ed5d45cc332cccc2eb0
deivid01 <deivid.ed.8669@gmail.com>
2026-02-28 20:07:03 -0300

    refactor: restructure repo to Vercel true zero-config monorepo

--- backend/products/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import ProdutoListView, ProdutoDetailView, CategoriaListView
-
-urlpatterns = [
-    path('', ProdutoListView.as_view(), name='produto-list'),
-    path('categorias/', CategoriaListView.as_view(), name='categoria-list'),
-    path('<slug:slug>/', ProdutoDetailView.as_view(), name='produto-detail'),
-]
